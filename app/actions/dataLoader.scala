803d13204a803ddd4cba8484767b4c44b0aaeedb
Madzia <magda2609@gmail.com>
2015-04-12 20:24:22 +0200

    load all files that was updated in last day of each month

--- app/actions/dataLoader.scala
+++ app/actions/dataLoader.scala
@@ -14,6 +14,10 @@ import scala.xml.XML
 import scala.io.Codec
 import org.apache.commons.lang3.CharSet
 import java.io.{File=>_, _}
+import java.text.SimpleDateFormat
+import org.joda.time.DateTime
+import scala.util.matching.Regex
+import collection.mutable.HashSet
 
 object dataLoader {
    def SelectRowFromExchangeRateSummary(temp: models.ExchangeRateSummary) = from(models.playDatabase.ExchangeRateSummaryTable){p => where(p.name === temp.name and p.coderate === temp.coderate)select(p)}
@@ -22,18 +26,32 @@ object dataLoader {
   def loadIntoDatabase() {
     
     val holmesUrl = """http://www.nbp.pl/kursy/xml/dir.txt"""
+     val Regex = """[a-b][0-9a-z]{4}((0[7-9])|(1[0-4]))[0-9]{2}(2[8-9]|(3[0-1]))""".r
+     val lista =  HashSet[String]()
+     val last_days = List()
       try{
         transaction{
            for (line <- Source.fromURL(holmesUrl).getLines) {
-            //   line match { case "[a-b][0-9a-z]{4}[0(7-9)|1(0-4)][0-9]{2}31" => add(fileUrl.replaceAll("code", line)) case _ => 0 }
-              line match { case "a069z020409" => 
-                {
-                  val string = fileUrl+line +".xml"
-                  add(string)
-                }
-                case _ => 0 }
+              line match { case Regex(_*) => lista.add(line) case _ => 0 }
            }
         }
+        val listOfBad =  HashSet[String]() 
+        lista.map(elem => {
+          lista.map ( v => {
+            if (v.substring(5,9) == elem.substring(5,9)) {
+              if (v.substring(10) < elem.substring(10)) {
+                listOfBad.add(v)
+              }
+              else if (v.substring(10) > elem.substring(10))
+                listOfBad.add(elem)
+            }
+          })
+        })
+        
+        listOfBad.map { x => lista.remove(x) }
+        
+        lista.map { x => add(fileUrl+x +".xml") }
+        
      }catch {
         case ex: Exception => Logger.info(ex.toString())
      }
@@ -59,7 +77,10 @@ object dataLoader {
                 if (temp.toList.length == 0) {
                   val x = actions.databaseFunction.insertDataToDatabase(listOfSummary)
                 }
-                actions.databaseFunction.updateMinValueDataToDatabase(listOfSummary, 0.42)
+                else {
+                  
+                }
+                //actions.databaseFunction.updateMinValueDataToDatabase(listOfSummary, 0.42)
            
 //                else {
 //                  temp
